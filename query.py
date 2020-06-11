5bf3ecec17c005f3de04f7f7b42452d3bb121b98
jaeheehur <64954518+jaeheehur@users.noreply.github.com>
2020-06-11 10:03:30 +0900

    Create query.py

---
+++ query.py
@@ -0,0 +1,48 @@
+import pymysql
+import pandas as pd
+
+def db_query(db, sql, params): 
+    # Connect to MySQL
+    conn = pymysql.connect(host='*ipname*', user='*userid*', password='*userpasswd*', charset='utf8', db=db,
+                           cursorclass=pymysql.cursors.DictCursor)
+    try: 
+        # create Dictionary Cursor 
+        with conn.cursor() as cursor: 
+            sql_query = sql 
+            
+            # excute SQL 
+            cursor.execute(sql_query, params) 
+            rows = cursor.fetchall()
+            return rows
+
+        conn.commit()
+        
+    finally: 
+        conn.close()
+        
+# Execute query and save the result as Dataframe format
+def query_executor(sql):
+    query = db_query(db='*dbname*', sql=sql, params=None)
+    query_df = pd.DataFrame(query)
+
+    return query_df
+    
+# Convert list to str for the purpose of query input format
+def item_in(list): 
+    items = "', '".join(map(str, list)) # where * IN ( 'items' )
+    return items
+
+def item_regexp(list):
+    items = "|".join(map(str, list)) # where * regexp 'items'
+    return items
+    
+def code_extractor(df, cdnm, param):
+    cd = df[cdnm].dropna() # drop NaN in dataframe
+    cd_item = item_in(cd) if param == 0 else item_regexp(cd) # item_in = 0, item_regexp = 1
+    return cd_item
+
+def unique_sort(df, colnm, boolean, uniquenm, keep_param):
+    df = df.sort_values(colnm, ascending=boolean)
+    unique_df = df.drop_duplicates([uniquenm], keep=keep_param)
+    return unique_df
+
